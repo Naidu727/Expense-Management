6e183f1f34ccd5d1ce0eeb56b88c75c0ec521f39
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 02:21:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 //middlewares
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 app.use(morgan('dev'));
