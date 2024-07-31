8054779b5eaf5e0ffa8e126559fa2ac86701ddda
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 02:44:43 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,57 +1,41 @@
-import React,{useState,useEffect} from 'react'
-import {Form,Input,message} from "antd"
-import { Link,useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import Password from 'antd/es/input/Password'
-import "../styles/LoginPage.css"
+import React, { useState, useEffect } from 'react';
+import { Form, Input, message, Button, Spin } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import Password from 'antd/es/input/Password';
+import "../styles/LoginPage.css";
+import axios from 'axios';
+
 const Login = () => {
-    const img =
-    "https://juntrax.com/blog/wp-content/uploads/2020/10/Expenses-Management-System-Featured-Juntrax.jpg";
-    const [Loading,setLoading] = useState(false)
-    const navigate = useNavigate()
-    const submitHandler = async(values)=>{
-        try{
-            setLoading(true)
-            const {data} = await axios.post('/users/login',values)
-            setLoading(false)
-            message.success('login success')
-            localStorage.setItem("user",
-            JSON.stringify({...data.user,password:""}))
-            navigate('/')
-        }
-        catch(error){
-            setLoading(false)
-            message.error('Something went wrong')
-        }
-    };
-    useEffect(()=>{
-        if(localStorage.getItem("user")){
-            navigate("/")
-        }
-    },[navigate])
+  const img = "https://juntrax.com/blog/wp-content/uploads/2020/10/Expenses-Management-System-Featured-Juntrax.jpg";
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post('/api/v1/users/login', values);
+      setLoading(false);
+      message.success('Login successful');
+      localStorage.setItem("user", JSON.stringify({ ...data.user, password: "" }));
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      message.error('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return (
     <>
-        
-        {/* <div className="register-page">
-        {Loading && <spinner/>}
-        <Form layout='vertical' onFinish={submitHandler}>
-        <h1>Login Form</h1>
-            <Form.Item label="Email" name="email">
-                <Input type="email"/> 
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-                <Input type="password"/> 
-            </Form.Item>
-            <div className='d-flex justify-content-between'>
-                <Link to = '/register' >Not a user ? Click Here to register</Link>
-                <button className='btn btn-primary'>Login</button>
-            </div>
-        </Form>
-        </div> */}
-        <div className="login-page ">
-        {Loading && <spinner />}
+      <div className="login-page">
+        {Loading && <Spin />}
         <div className="row container">
-          <h1 className='text-exp'>Expense Managment System</h1>
+          <h1 className='text-exp'>Expense Management System</h1>
           <div className="col-md-6">
             <img src={img} alt="login-img" width={"100%"} height="100%" />
           </div>
@@ -63,20 +47,20 @@ const Login = () => {
                 <Input type="email" required />
               </Form.Item>
               <Form.Item label="Password" name="password">
-                <Input type="password" required />
+                <Password required />
               </Form.Item>
               <div className="d-flex justify-content-between">
                 <Link to="/register">
-                  Not a user ? Click Here to regsiter !
+                  Not a user? Click Here to register!
                 </Link>
-                <button className="btn">Login</button>
+                <Button type="primary" htmlType="submit">Login</Button>
               </div>
             </Form>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
