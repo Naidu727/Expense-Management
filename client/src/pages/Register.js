0295b8310b5906888b85ef7c98efb6a5f696b62a
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 17:50:36 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,54 +1,69 @@
-import React,{useState,useEffect} from 'react'
-import {Form,Input,message} from "antd"
-import { Link,useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import Spinner from '../components/Layout/Spinner'
-import "../styles/RegisterPage.css"
-const Register = () => {
-    const navigate = useNavigate()
-    const [Loading,setLoading] = useState(false)
-    const submitHandler = async(values)=>{
-        try{
-            setLoading(true)
-            await axios.post('/users/register',values)
-            message.success('Registration Successful')
-            setLoading(false)
-            navigate('/login')
-        }
-        catch(error){
-            setLoading(false)
-            message.error('Something went wrong');
-        }
-    };
-    useEffect(()=>{
-        if(localStorage.getItem("user")){
-            navigate("/")
-        }
-    },[navigate])
-  return (
-    <>
-    
-        <div className="register-page">
-        {Loading &&<Spinner/>}
-        <Form className='register-form' layout='vertical' onFinish={submitHandler}>
-        <h1>Register Form</h1>
-            <Form.Item label="Name" name="name">
-                <Input/> 
-            </Form.Item>
-            <Form.Item label="Email" name="email">
-                <Input type="email"/> 
-            </Form.Item>
-            <Form.Item label="Password" name="password">
-                <Input type="password"/> 
-            </Form.Item>
-            <div className='d-flex justify-content-between'>
-                <Link to = '/login' >Already Registered ? Click Here to login</Link>
-                <button className='btn btn-primary'>Register</button>
-            </div>
-        </Form>
-        </div>
-    </>
-  )
-}
+import React, { useState, useEffect } from 'react';
+import { Form, Input, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Spinner from '../components/Layout/Spinner';
+import '../styles/RegisterPage.css';
 
-export default Register
+const Register = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (values) => {
+    try {
+      setLoading(true);
+      await axios.post('/api/v1/users/register', values);
+      message.success('Registration Successful');
+      setLoading(false);
+      navigate('/login');
+    } catch (error) {
+      setLoading(false);
+      message.error('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      navigate('/');
+    }
+  }, [navigate]);
+
+  return (
+    <div className="register-page">
+      {loading && <Spinner />}
+      <Form className="register-form" layout="vertical" onFinish={submitHandler}>
+        <h1>Register Form</h1>
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Please input your email!' },
+            { type: 'email', message: 'The input is not valid E-mail!' },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input type="password" />
+        </Form.Item>
+        <div className="d-flex justify-content-between">
+          <Link to="/login">Already Registered? Click Here to login</Link>
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
