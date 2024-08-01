9f3a327ce5ae52b8d29a7b907d5c9a7f19d21201
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 17:14:07 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,49 @@
 const userModel = require("../models/userModel");
+const bcrypt = require('bcryptjs');
 
-const loginController  = async(req,res)=>{
-    try{
-        const {email,password} = req.body;
-        const user = await userModel.findOne({email,password});
-        if(!user)
-        {
+const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+        if (!user) {
             return res.status(404).send("User Not Found");
         }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid Credentials");
+        }
         res.status(200).json({
-            success:true,
+            success: true,
             user,
         });
-    }catch(error){
-        res.status(400).json({
-            success :false,
+    } catch (error) {
+        res.status(500).json({
+            success: false,
             error,
-        })
+        });
     }
 }
 
-const registerController = async(req,res)=>{
-    try{
-        const newUser = new userModel(req.body);
+const registerController = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send("User already exists");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new userModel({ name, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json({
-            success :true,
+            success: true,
             newUser,
         });
-    }
-    catch(error){
-        res.status(400).json({
-            success:false,
+    } catch (error) {
+        res.status(500).json({
+            success: false,
             error,
         });
     }
 }
 
-module.exports = {loginController,registerController};
+module.exports = { loginController, registerController };
