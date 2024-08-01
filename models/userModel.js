bf65da03c68c3b35239ac15a3d7cccd947bd2309
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 17:14:34 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
-const { type } = require("os");
-const userSchema = new mongoose.Schema(
-    {
-    name:{
-        type:String,
-        required:[true,"name is required"],
-    },
-    email:{
-        type:String,
-        required:[true,"email is required and should be unique"],
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:[true,"password is required"],
-    },
-},{timestamps:true});
 
-const userModel = mongoose.model("users",userSchema);
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required and should be unique"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+}, { timestamps: true });
+
+const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
