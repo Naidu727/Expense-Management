f0d7b93e0836c6ab33a690cba1af03f20abb83b0
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-01 16:37:51 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -188,3 +188,44 @@ const HomePage = () => {
         open={showModal}
         onCancel={() => setShowModal(false)}
         footer={false}
+      >
+        <Form layout='vertical' onFinish={handleSubmit} initialValues={editable}>
+          <Form.Item label="Amount" name="amount">
+            <Input type='text' />
+          </Form.Item>
+          <Form.Item label="Type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Category" name="Category">
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="tip">Tip</Select.Option>
+              <Select.Option value="project">Project</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="movie">Movie</Select.Option>
+              <Select.Option value="bills">Bills</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="fee">Fee</Select.Option>
+              <Select.Option value="tax">Tax</Select.Option>
+              <Select.Option value="other">Other</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type='date' />
+          </Form.Item>
+          <Form.Item label="Description" name='description'>
+            <Input type='text' />
+          </Form.Item>
+          <div className='d-flex justify-content-end'>
+            <button className='btn btn-primary' type='submit'>SAVE</button>
+          </div>
+        </Form>
+      </Modal>
+    </Layout>
+  );
+}
+
+export default HomePage;
