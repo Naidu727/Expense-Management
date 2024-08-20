4002988493b0fb65282dad724a709f9c4d5f12e6
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-21 03:41:35 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,37 +40,50 @@
 // module.exports = {loginController,registerController};
 
 
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
+const userModel = require("../models/userModel");
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required"],
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required and should be unique"],
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: [true, "Password is required"],
-        },
-    },
-    { timestamps: true }
-);
+const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
 
-// Pre-save hook to hash the password before saving
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        return next();
+        if (!user) {
+            return res.status(404).send("User Not Found");
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid Credentials");
+        }
+
+        res.status(200).json({
+            success: true,
+            user,
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error,
+        });
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
+};
 
-const userModel = mongoose.model("users", userSchema);
-module.exports = userModel;
+const registerController = async (req, res) => {
+    try {
+        const newUser = new userModel(req.body);
+        await newUser.save();
+        res.status(201).json({
+            success: true,
+            newUser,
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error,
+        });
+    }
+};
+
+module.exports = { loginController, registerController };
+
