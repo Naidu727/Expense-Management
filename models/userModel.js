1c107f8b278d63b4327246eff4c884e0f4ed9d3f
Naidu727 <130582105+Naidu727@users.noreply.github.com>
2024-08-21 03:42:01 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,56 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: [true, "Name is required"],
+//     },
+//     email: {
+//         type: String,
+//         required: [true, "Email is required and should be unique"],
+//         unique: true,
+//     },
+//     password: {
+//         type: String,
+//         required: [true, "Password is required"],
+//     },
+// }, { timestamps: true });
+
+// const userModel = mongoose.model("User", userSchema);
+// module.exports = userModel;
+
+
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Name is required"],
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required and should be unique"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
     },
-    email: {
-        type: String,
-        required: [true, "Email is required and should be unique"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-    },
-}, { timestamps: true });
+    { timestamps: true }
+);
 
-const userModel = mongoose.model("User", userSchema);
+// Pre-save hook to hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const userModel = mongoose.model("users", userSchema);
 module.exports = userModel;
